56e4f8b5e5091d59f7c3bfcb814cd40c7bb2c907
Zenkai-Jay <JESURUN@email.sc.edu>
2024-12-03 13:19:54 -0500

    Add files via upload

---
+++ Lab01.java
@@ -0,0 +1,16 @@
+
+public class Lab01 {
+
+	public static void main(String[] args) {
+		//Jesurun Marshall
+		
+		
+		System.out.println(" _   _     _   _      ");
+		System.out.println("( `^`))   ( `^`))     ");
+		System.out.println("|    ||   |    ||     ");
+		System.out.println("|    ||   |    ||     ");
+		System.out.println("'----'`   '----'`     ");
+
+	}
+
+}
